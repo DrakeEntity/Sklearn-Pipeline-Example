24caafc8c4248f1e238400782ad7f957a04f02e6
drakeentity <deepak9818183049@gmail.com>
2020-05-18 16:10:59 +0530

    sklearn pipeline example added

---
+++ Sklearn Pipeline.py
@@ -0,0 +1,18 @@
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+
+X, y = make_classification(random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
+
+steps = [
+    ('scaler', StandardScaler()), 
+    ('SVM', SVC())
+    ]
+
+pipe = Pipeline(steps)
+
+pipe.fit(X_train, y_train)
+print(pipe.score(X_test, y_test))
